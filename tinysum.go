b40f508b36eb245814147e9673641176d5b6217a
Matt Keller <m@cognusion.com>
2016-03-16 22:16:41 -0400

    Adds "64" bit versions to each of the commands, to use CRC64 in lieu of CRC32

--- tinysum.go
+++ tinysum.go
@@ -4,40 +4,73 @@ package tinysum
 import (
 	"fmt"
 	"hash/crc32"
+	"hash/crc64"
 )
+
+// Returns the 32bit sum of b
+func Sum32(b []byte) (sum uint64) {
+	h := crc32.NewIEEE()
+	h.Write(b)
+	sum = uint64(h.Sum32())
+	return
+}
+
+// Returns the 64bit sum of b
+func Sum64(b []byte) (sum uint64) {
+	sum = crc64.Checksum(b, crc64.MakeTable(crc64.ECMA))
+	return
+}
 
 // Returns an offset CRC32 of the provided string
 func OffsetStringSum(s string, offset uint32) (tiny string) {
-	h := crc32.NewIEEE()
-	h.Write([]byte(s))
-	s64 := uint64(h.Sum32() + offset)
-	tiny = fmt.Sprintf("%x", s64)
+	tiny = OffsetByteSum([]byte(s), offset)
+	return
+}
+
+// Returns an offset CRC64 of the provided string
+func OffsetStringSum64(s string, offset uint32) (tiny string) {
+	tiny = OffsetByteSum64([]byte(s), offset)
 	return
 }
 
 // Returns an offset CRC32 of the provided []byte
 func OffsetByteSum(b []byte, offset uint32) (tiny string) {
-	h := crc32.NewIEEE()
-	h.Write(b)
-	s64 := uint64(h.Sum32() + offset)
+	s64 := Sum32(b) + uint64(offset)
+	tiny = fmt.Sprintf("%x", s64)
+	return
+}
+
+// Returns an offset CRC64 of the provided []byte
+func OffsetByteSum64(b []byte, offset uint32) (tiny string) {
+	s64 := Sum64(b) + uint64(offset)
 	tiny = fmt.Sprintf("%x", s64)
 	return
 }
 
 // Returns a CRC32 of the provided string
 func StringSum(s string) (tiny string) {
-	h := crc32.NewIEEE()
-	h.Write([]byte(s))
-	s64 := uint64(h.Sum32())
+	s64 := Sum32([]byte(s))
+	tiny = fmt.Sprintf("%x", s64)
+	return
+}
+
+// Returns a CRC64 of the provided string
+func StringSum64(s string) (tiny string) {
+	s64 := Sum64([]byte(s))
 	tiny = fmt.Sprintf("%x", s64)
 	return
 }
 
 // Returns a CRC32 of the provided []byte
 func ByteSum(b []byte) (tiny string) {
-	h := crc32.NewIEEE()
-	h.Write(b)
-	s64 := uint64(h.Sum32())
+	s64 := Sum32(b)
+	tiny = fmt.Sprintf("%x", s64)
+	return
+}
+
+// Returns a CRC64 of the provided []byte
+func ByteSum64(b []byte) (tiny string) {
+	s64 := Sum64(b)
 	tiny = fmt.Sprintf("%x", s64)
 	return
 }
