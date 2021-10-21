1e134915bbdd54a082ec94a9b246c29fca12bd66
M <m@cognusion.com>
2021-10-21 11:40:10 -0400

    Fixes all of the docomments

--- tinysum.go
+++ tinysum.go
@@ -1,4 +1,4 @@
-// Make a CRC32 checksum (with or without an offset) of a string or []byte
+// Package tinysum provides a number of quazi-primitives to assist with 32 and 64-bit CRC sums
 package tinysum
 
 import (
@@ -7,7 +7,7 @@ import (
 	"hash/crc64"
 )
 
-// Returns the 32bit sum of b
+// Sum32 returns the 32bit sum of b
 func Sum32(b []byte) (sum uint64) {
 	h := crc32.NewIEEE()
 	h.Write(b)
@@ -15,60 +15,60 @@ func Sum32(b []byte) (sum uint64) {
 	return
 }
 
-// Returns the 64bit sum of b
+// Sum64 returns the 64bit sum of b
 func Sum64(b []byte) (sum uint64) {
 	sum = crc64.Checksum(b, crc64.MakeTable(crc64.ECMA))
 	return
 }
 
-// Returns an offset CRC32 of the provided string
+// OffsetStringSum returns an offset CRC32 of the provided string
 func OffsetStringSum(s string, offset uint32) (tiny string) {
 	tiny = OffsetByteSum([]byte(s), offset)
 	return
 }
 
-// Returns an offset CRC64 of the provided string
+// OffsetStringSum64 returns an offset CRC64 of the provided string
 func OffsetStringSum64(s string, offset uint32) (tiny string) {
 	tiny = OffsetByteSum64([]byte(s), offset)
 	return
 }
 
-// Returns an offset CRC32 of the provided []byte
+// OffsetByteSum returns an offset CRC32 of the provided []byte
 func OffsetByteSum(b []byte, offset uint32) (tiny string) {
 	s64 := Sum32(b) + uint64(offset)
 	tiny = fmt.Sprintf("%x", s64)
 	return
 }
 
-// Returns an offset CRC64 of the provided []byte
+// OffsetByteSum64 returns an offset CRC64 of the provided []byte
 func OffsetByteSum64(b []byte, offset uint32) (tiny string) {
 	s64 := Sum64(b) + uint64(offset)
 	tiny = fmt.Sprintf("%x", s64)
 	return
 }
 
-// Returns a CRC32 of the provided string
+// StringSum returns a CRC32 of the provided string
 func StringSum(s string) (tiny string) {
 	s64 := Sum32([]byte(s))
 	tiny = fmt.Sprintf("%x", s64)
 	return
 }
 
-// Returns a CRC64 of the provided string
+// StringSum64 returns a CRC64 of the provided string
 func StringSum64(s string) (tiny string) {
 	s64 := Sum64([]byte(s))
 	tiny = fmt.Sprintf("%x", s64)
 	return
 }
 
-// Returns a CRC32 of the provided []byte
+// ByteSum returns a CRC32 of the provided []byte
 func ByteSum(b []byte) (tiny string) {
 	s64 := Sum32(b)
 	tiny = fmt.Sprintf("%x", s64)
 	return
 }
 
-// Returns a CRC64 of the provided []byte
+// ByteSum64 returns a CRC64 of the provided []byte
 func ByteSum64(b []byte) (tiny string) {
 	s64 := Sum64(b)
 	tiny = fmt.Sprintf("%x", s64)
